4757f7c8482cf6dfe26449ce15fe6c467746f294
Aaron Hernandez <darkfire2369@outlook.com>
2024-10-16 16:59:45 -0600

    Revert "Merge branch 'Gil' into Prueba2"
    This reverts commit a934ecdd28b6fb2fff4bcb6236b65b1442f158c5, reversing
    changes made to 4d3e33aee90a18b7a3a64e33e16cf42e19901500.

--- Reto/app/src/main/java/com/example/reto/MainActivity.kt
+++ Reto/app/src/main/java/com/example/reto/MainActivity.kt
@@ -3,14 +3,20 @@ package com.example.reto
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+
+
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.reto.components.NavegacionInferior
+import com.example.reto.navigation.AppNavigation
 import com.example.reto.ui.theme.RetoTheme
 import com.example.reto.vista.HomeScreen
 import com.example.reto.vista.IntroScreen
@@ -19,14 +25,13 @@ import com.example.reto.vista.StartScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             RetoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    MainScreen()
                 }
             }
         }
@@ -34,17 +39,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            NavegacionInferior(navController)
+        }
+    ) {padding->
+        Box(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize()
+        ){
+            AppNavigation(navController = navController)
+        }
+
+    }
+
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    RetoTheme {
-        Greeting("Android")
-    }
+fun PreviewComponent(){
+    MainScreen()
 }
--- Reto/app/src/main/java/com/example/reto/Vista/Welcome.kt
+++
@@ -1,2 +0,0 @@
-package com.example.reto.Vista
-
