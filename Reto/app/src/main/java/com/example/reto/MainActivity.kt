a01a11fe021b0d0d3d510dc258ccba98a938a61a
Aaron Hernandez <darkfire2369@outlook.com>
2024-10-16 16:05:59 -0600

    ArregléNombres

--- Reto/app/src/main/java/com/example/reto/MainActivity.kt
+++ Reto/app/src/main/java/com/example/reto/MainActivity.kt
@@ -18,6 +18,9 @@ import androidx.navigation.compose.rememberNavController
 import com.example.reto.components.NavegacionInferior
 import com.example.reto.navigation.AppNavigation
 import com.example.reto.ui.theme.RetoTheme
+import com.example.reto.vista.HomeScreen
+import com.example.reto.vista.IntroScreen
+import com.example.reto.vista.StartScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
